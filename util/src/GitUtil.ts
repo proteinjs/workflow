e57b7e789a4e26e9bbf177d08ca0b7e5d5522de8
Brent Bahry <brentbahry@gmail.com>
2023-09-27 20:54:58 -0700

    added git functions

--- util/src/GitUtil.ts
+++ util/src/GitUtil.ts
@@ -14,6 +14,83 @@ export class GitUtil {
     await cmd('git', args, envVars);
     GitUtil.LOGGER.info(`Ran command: ${command}`);
   }
+
+  static async init(directory: string): Promise<void> {
+    const args = ['init'];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
+
+  static async setRemote(directory: string, remote: string): Promise<void> {
+    const args = ['remote', 'set-url', 'origin', remote];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
+
+  static async addRemote(directory: string, remote: string): Promise<void> {
+    const args = ['remote', 'add', 'origin', remote];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
+
+  static async commit(directory: string, message: string): Promise<void> {
+    const args = ['commit', '-m', message];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
+
+  static async pull(directory: string): Promise<void> {
+    const args = ['pull'];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
+
+  static async push(directory: string): Promise<void> {
+    const args = ['push'];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
+
+  static async status(directory: string): Promise<void> {
+    const args = ['status'];
+    const command = 'git ' + args.join(' ');
+    let envVars;
+    if (directory)
+      envVars = { cwd: directory }
+    GitUtil.LOGGER.info(`Running command: ${command}`);
+    await cmd('git', args, envVars);
+    GitUtil.LOGGER.info(`Ran command: ${command}`);
+  }
 }
 
 export const cloneAppTemplatePackagesFunctionName = 'cloneAppTemplatePackages';
@@ -33,4 +110,149 @@ export const cloneAppTemplatePackagesFunction = {
     },
   },
   call: async (params: { directory: string }) => await GitUtil.cloneAppTemplatePackages(params.directory),
+}
+
+export const initFunctionName = 'gitInit';
+export const initFunction = {
+  definition: {
+    name: initFunctionName,
+    description: 'Initialize a new git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory to initialize the git repository in',
+        },
+      },
+      required: ['directory']
+    },
+  },
+  call: async (params: { directory: string }) => await GitUtil.init(params.directory),
+}
+
+export const setRemoteFunctionName = 'gitSetRemote';
+export const setRemoteFunction = {
+  definition: {
+    name: setRemoteFunctionName,
+    description: 'Set the remote of the git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory of the git repository',
+        },
+        remote: {
+          type: 'string',
+          description: 'The remote to set',
+        },
+      },
+      required: ['directory', 'remote']
+    },
+  },
+  call: async (params: { directory: string, remote: string }) => await GitUtil.setRemote(params.directory, params.remote),
+}
+
+export const addRemoteFunctionName = 'gitAddRemote';
+export const addRemoteFunction = {
+  definition: {
+    name: addRemoteFunctionName,
+    description: 'Add a remote to the git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory of the git repository',
+        },
+        remote: {
+          type: 'string',
+          description: 'The remote to add',
+        },
+      },
+      required: ['directory', 'remote']
+    },
+  },
+  call: async (params: { directory: string, remote: string }) => await GitUtil.addRemote(params.directory, params.remote),
+}
+
+export const commitFunctionName = 'gitCommit';
+export const commitFunction = {
+  definition: {
+    name: commitFunctionName,
+    description: 'Commit changes in the git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory of the git repository',
+        },
+        message: {
+          type: 'string',
+          description: 'The commit message',
+        },
+      },
+      required: ['directory', 'message']
+    },
+  },
+  call: async (params: { directory: string, message: string }) => await GitUtil.commit(params.directory, params.message),
+}
+
+export const pullFunctionName = 'gitPull';
+export const pullFunction = {
+  definition: {
+    name: pullFunctionName,
+    description: 'Pull changes from the remote of the git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory of the git repository',
+        },
+      },
+      required: ['directory']
+    },
+  },
+  call: async (params: { directory: string }) => await GitUtil.pull(params.directory),
+}
+
+export const pushFunctionName = 'gitPush';
+export const pushFunction = {
+  definition: {
+    name: pushFunctionName,
+    description: 'Push changes to the remote of the git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory of the git repository',
+        },
+      },
+      required: ['directory']
+    },
+  },
+  call: async (params: { directory: string }) => await GitUtil.push(params.directory),
+}
+
+export const statusFunctionName = 'gitStatus';
+export const statusFunction = {
+  definition: {
+    name: statusFunctionName,
+    description: 'Get the status of the git repository in the specified directory',
+    parameters: {
+      type: 'object',
+      properties: {
+        directory: {
+          type: 'string',
+          description: 'The directory of the git repository',
+        },
+      },
+      required: ['directory']
+    },
+  },
+  call: async (params: { directory: string }) => await GitUtil.status(params.directory),
 }
