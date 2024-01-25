b9b5775f4ce00ae8771f00c4daa4c1b10a3183e3
Brent Bahry <brentbahry@gmail.com>
2024-01-25 01:42:52 -0800

    testing globby directly

--- build/src/buildRepo.ts
+++ build/src/buildRepo.ts
@@ -2,37 +2,42 @@ import * as path from 'path'
 const graphlib = require('@dagrejs/graphlib')
 import { PackageUtil, cmd } from '@brentbahry/util-server'
 import { Logger } from '@brentbahry/util'
+import globby from 'globby'
 
 export const buildRepo = async () => {
   const logger = new Logger('buildRepo', 'debug');
   logger.info(`> Building proteinjs workspace`);
   const repoPath = path.resolve(__dirname, '../../..'); // __dirname: build/dist/src/
   logger.debug(`repoPath: ${repoPath}`);
-  const packageMap = await PackageUtil.getLocalPackageMap(repoPath, ['**/test/**']);
-  logger.debug(`packageMap:\n${JSON.stringify(packageMap, null, 2)}`, true);
-  const packageGraph = await PackageUtil.getPackageDependencyGraph(Object.values(packageMap).map(localPackage => localPackage.packageJson));
-  const sortedPackageNames: string[] = graphlib.alg.topsort(packageGraph).reverse();
-  logger.debug(`sortedPackageNames:\n${JSON.stringify(sortedPackageNames, null, 2)}`, true);
+  const paths = await globby(repoPath + '/' + '**/package.json', {
+    ignore: ['**/node_modules/**', '**/dist/**']
+  });
+  logger.debug(`paths:\n${JSON.stringify(paths, null, 2)}`, true);
+  // const packageMap = await PackageUtil.getLocalPackageMap(repoPath, ['**/test/**']);
+  // logger.debug(`packageMap:\n${JSON.stringify(packageMap, null, 2)}`, true);
+  // const packageGraph = await PackageUtil.getPackageDependencyGraph(Object.values(packageMap).map(localPackage => localPackage.packageJson));
+  // const sortedPackageNames: string[] = graphlib.alg.topsort(packageGraph).reverse();
+  // logger.debug(`sortedPackageNames:\n${JSON.stringify(sortedPackageNames, null, 2)}`, true);
 
-  logger.info(`> Installing packages`);
-  for (let packageName of sortedPackageNames) {
-    const localPackage = packageMap[packageName];
-    if (!localPackage)
-      continue;
+  // logger.info(`> Installing packages`);
+  // for (let packageName of sortedPackageNames) {
+  //   const localPackage = packageMap[packageName];
+  //   if (!localPackage)
+  //     continue;
 
-    const packageDir = path.dirname(localPackage.filePath);
-    await cmd('npm', ['install'], { cwd: packageDir });
-    logger.info(`Installed ${packageName}`);
-  }
+  //   const packageDir = path.dirname(localPackage.filePath);
+  //   await cmd('npm', ['install'], { cwd: packageDir });
+  //   logger.info(`Installed ${packageName}`);
+  // }
 
-  logger.info(`> Building packages`);
-  for (let packageName of sortedPackageNames) {
-    const localPackage = packageMap[packageName];
-    if (!localPackage)
-      continue;
+  // logger.info(`> Building packages`);
+  // for (let packageName of sortedPackageNames) {
+  //   const localPackage = packageMap[packageName];
+  //   if (!localPackage)
+  //     continue;
 
-    const packageDir = path.dirname(localPackage.filePath);
-    await cmd('npm', ['run', 'build'], { cwd: packageDir });
-    logger.info(`Built ${packageName}`);
-  }
+  //   const packageDir = path.dirname(localPackage.filePath);
+  //   await cmd('npm', ['run', 'build'], { cwd: packageDir });
+  //   logger.info(`Built ${packageName}`);
+  // }
 }
