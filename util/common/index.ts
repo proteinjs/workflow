abc5ab820d1f8b7415cfccf8a1ff7f669b855c00
Brent Bahry <brentbahry@gmail.com>
2024-02-25 19:01:17 -0800

    export Graph from util/common

--- util/common/index.ts
+++ util/common/index.ts
@@ -1,4 +1,5 @@
 export * from './src/graph/graphSerializer';
+export * from './src/graph/graph';
 export * from './src/jsonCycle';
 export * from './src/Logger';
 export * from './src/ansiiColors';
---
+++ util/common/src/graph/graph.ts
@@ -0,0 +1,1 @@
+export { Graph } from '@dagrejs/graphlib';
