d690a347acaf9c5a6dab406726d9044d1f48e070
Brent Bahry <brentbahry@gmail.com>
2024-04-02 16:48:24 -0700

    scoped tables

--- workflow/common/src/tables/WorkflowExecutionTable.ts
+++ workflow/common/src/tables/WorkflowExecutionTable.ts
@@ -1,8 +1,9 @@
-import { Table, StringColumn, Record, withRecordColumns, Reference, ReferenceColumn } from '@proteinjs/db';
+import { Table, StringColumn, Reference, ReferenceColumn } from '@proteinjs/db';
+import { ScopedRecord, withScopedRecordColumns } from '@proteinjs/user';
 import { WorkflowStep, WorkflowStepTable } from './WorkflowStepTable';
 import { Workflow, WorkflowTable } from './WorkflowTable';
 
-export interface WorkflowExecution extends Record {
+export interface WorkflowExecution extends ScopedRecord {
 	workflow: Reference<Workflow>;
   currentStep: Reference<WorkflowStep>;
   status: 'pending'|'active'|'success'|'failure';
@@ -10,7 +11,7 @@ export interface WorkflowExecution extends Record {
 
 export class WorkflowExecutionTable extends Table<WorkflowExecution> {
 	public name = 'workflow_execution';
-	public columns = withRecordColumns<WorkflowExecution>({
+	public columns = withScopedRecordColumns<WorkflowExecution>({
 		workflow: new ReferenceColumn<Workflow>('workflow', new WorkflowTable().name, false),
     currentStep: new ReferenceColumn<WorkflowStep>('current_step', new WorkflowStepTable().name, false),
     status: new StringColumn('status', { defaultValue: async () => 'pending' }),
