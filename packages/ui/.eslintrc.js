bdf227ece450201ad07575dfa95dc9d3f65e46d3
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-05-09 11:40:43 -0700

    update settings json

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   root: true,
