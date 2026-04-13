4407dd4e74f20d04119b4a93e84baff0d3818b60
Brent Bahry <brentbahry@gmail.com>
2026-04-13 12:34:18 -0700

    chore: bump version

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -2,3 +2,5 @@ export * from './src/tables/tables';
 export * from './src/tables/WorkflowTable';
 export * from './src/tables/WorkflowStepTable';
 export * from './src/tables/WorkflowExecutionTable';
+
+// bump
